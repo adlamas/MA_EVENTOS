88023d7df4e528aa560f3cda729af23fced22fe1
adlamas <alan12394@hotmail.com>
2017-03-26 18:56:42 -0300

    Login con PhP y MySql
    Login usando una base de datos MySql en localhost y Php

--- Controlador/ingreso.php
+++ Controlador/ingreso.php
@@ -12,8 +12,32 @@ and open the template in the editor.
     <body>
         <?php
         
-       echo "Conexión"
+        $nombre = $_POST[addslashes('nombre')];
+        $apellido = $_POST['apellido'];
+        $password = $_POST['password'];
+       
+        //echo $nombre;
         
+       try{ 
+       $conexion = new PDO('mysql:host=localhost;dbname=app_web','root','');
+       
+       }
+       catch(Exception $e){
+           echo $e->getMessage();
+       }
+       
+       $sql = "SELECT * FROM usuarios WHERE nombre = '".$nombre."'and password = '".$password."' ";
+       $resultados = $conexion->query($sql);
+       
+//       while($resultado = $resultados->fetch(PDO::FETCH_NUM)){
+//           echo '<div style="color: blue">Nombre: ' . $resultado[1] . '</div>' . 
+//                   '<div style="color: red"> Apellido: ' . $resultado[2] . '</div>' . '<br>';
+//       }
+     
+        if($resultados->fetch(PDO::FETCH_ASSOC)){
+            echo "Bienvenido/a señor/a " . $nombre; 
+        }else
+            echo "Ud. no se registró";
         ?>
     </body>
 </html>
